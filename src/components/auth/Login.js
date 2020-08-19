87cb93743c620808a416158d89f1d70e6840e245
mjggodoy <mjggodoy@gmail.com>
2020-08-19 20:21:50 +0200

    Create form for the app -> login/password in home page

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,8 +1,41 @@
 import React from 'react';
 
 const Login = () => {
+
+    const startSession = () => {
+    }
+
     return (
-        <h1>From Login</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>User Login</h1>
+                <form>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" 
+                            id="email" 
+                            name="email"
+                            placeholder="mjggodoy@gmail.com" 
+                            onchange={startSession}>
+                        </input>
+                    </div>
+                    
+                    <div className="campo-form">
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" 
+                            id="password" 
+                            name="password"
+                            placeholder="password" 
+                            onchange={startSession}>
+                        </input>
+                    </div>
+
+                    <div className="campo-form">
+                        <input type="submit" className="btn btn-primario btn-block" value="Start session"></input>
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
 export default Login;
