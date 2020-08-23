b665df1e30fd48ad4a9a7f924ba322df24900762
mjggodoy <mjggodoy@gmail.com>
2020-08-23 21:09:23 +0200

    Create state for new project form -> actions, states etc.

--- src/components/proyect/NewProject.js
+++ src/components/proyect/NewProject.js
@@ -1,14 +1,34 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 
 const NewProject = () => {
+    const [newProject, saveNewProject] = useState({
+        projectName : ""
+    });
+
+    const onChangeProject = e => {
+        saveNewProject({
+            ...newProject, 
+            [e.target.name] : e.target.value
+        });
+    }
+
+    const onSubmitProject = e => {
+        e.preventDefault();
+    }
+
+    const {projectName} = newProject;
+
     return(
         <Fragment>
             <button type="button" className="btn btn-block btn-primary">New project</button>
-            <form className="form-new-project">
+            <form className="form-new-project"
+                onSubmit = {onSubmitProject}>
                 <input type="input-text" 
                         className="input-text"
-                        name="projectname"
-                        placeholder="Add project name">
+                        value = {projectName}
+                        name="projectName"
+                        placeholder="Add project name"
+                        onChange={onChangeProject}>
                 </input>
                 <input type="submit" className="btn btn-primary btn-block" value="Add new project!"></input>
             </form>
