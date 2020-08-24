70ac312cfea2083f8bef8fee74a73b7343cdb4c3
mjggodoy <mjggodoy@gmail.com>
2020-08-25 00:59:11 +0200

    Create Header -> create a component header that includes a link to close session and a navigation bar.

---
+++ src/components/layout/Bar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const Bar = () => {
+    return (
+       <header className="app-header">
+            <p className="user-name">Welcome: <span>María García</span></p>
+            <nav className="nav-main">
+                <a href="/">Close session</a>
+            </nav>
+       </header>
+      
+    );
+}
+export default Bar;
--- src/components/proyect/Projects.js
+++ src/components/proyect/Projects.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import SideBar from './../layout/SideBar';
+import Bar from './../layout/Bar';
 
 const Projects = () => {
     return (
         <div className="contenedor-app">
             <SideBar/>
             <div className="seccion-principal">
+                <Bar/>
                 <main>
                     <div className="contenedor-tareas">
                     </div>
