2b178d744143d50936966f92377a58cdb758d885
mjggodoy <mjggodoy@gmail.com>
2020-08-26 00:16:38 +0200

    Check tasks list length. Some additional minor changes.

--- src/components/task/TaskList.js
+++ src/components/task/TaskList.js
@@ -10,7 +10,8 @@ const TaskList = () => {
 
     return (
         <ul className="task-list">
-            {tasks.map(task => {
+            {tasks.length === 0 ? (<li className="task"><p>There are no tasks</p></li>) :
+            tasks.map(task => {
                 return (<Task task = {task}/>
             )})}
         </ul>
