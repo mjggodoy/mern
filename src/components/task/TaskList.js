0abc7c3bbadc5d3b22a6fdd5072c0114f6b1691d
mjggodoy <mjggodoy@gmail.com>
2020-09-08 21:28:58 +0200

    Select a project from project list and project's name should appaear above list of tasks.

--- src/components/task/TaskList.js
+++ src/components/task/TaskList.js
@@ -1,16 +1,24 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useContext} from 'react';
 import Task from './Task';
+import projectContext from './../../context/projectos/projectContext';
 
 const TaskList = () => {
+    const projectsContext = useContext(projectContext);
+    const {project} = projectsContext;
     const tasks = [
         {name : "Task1", id: "1", status: "completed"}, 
         {name : "Task2", id: "2", status: "closed"},
         {name : "Task3", id: "3", status: "in progress"}
     ];
 
+    if (project === null) {
+        return <h2>Select a project</h2>;
+    }
+
+    const [currentProject] = project;
     return (
         <Fragment>
-            <h2>Project Maria</h2>
+            <h1>{currentProject.projectName}</h1>
             <ul className="task-list">
                 {tasks.length === 0 ? (<li className="task"><p>There are no tasks</p></li>) :
                 tasks.map(task => {
