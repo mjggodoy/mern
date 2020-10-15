c17b00f3109e1b599ec7715aa21cf0441a4f4be6
Maria <mjggodoy@gmail.com>
2020-10-15 21:35:48 +0200

    Setting server and testin it.

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
-console.log('From index.js');
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+    res.send('Hi, world');
+});
+
+app.listen(PORT, () => {
+    console.log(`The app is running at this ${PORT}`);
+})
