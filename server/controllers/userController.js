4bdfad02b65f6f2160da19fddb09f4170e405232
Maria <mjggodoy@gmail.com>
2020-10-16 21:56:40 +0200

    Include express-validator lib to do valitations in API. Use checks in the router and catch errors in usercontroller.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,14 @@
 const User = require('../models/User');
 const bcryptjs = require('bcryptjs');
+const {validationResult} = require('express-validator');
 
 exports.createUser = async (req, res) => {
-    const {userName, email, password} = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {email, password} = req.body;
     try {
         const userEmail = await User.findOne({email});
         if (userEmail) {
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('./../controllers/userController');
+const {check} = require('express-validator');
 
 router.post('/',
+    [
+        check('userName', 'The username is mandatory').notEmpty(),
+        check('email', 'The email should be a valid email').isEmail(),
+        check('password', 'The password should contain at least 6 characters').isLength({min: 6})
+    ],
     userController.createUser
 );
 
