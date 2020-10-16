1d5113912592e4db1d43e639d6536905779804d8
Maria <mjggodoy@gmail.com>
2020-10-16 21:53:14 +0200

    Encrypt users'passwords using bcryptjs lib

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,13 +1,17 @@
 const User = require('../models/User');
+const bcryptjs = require('bcryptjs');
 
 exports.createUser = async (req, res) => {
-    const {userName, email} = req.body;
+    const {userName, email, password} = req.body;
     try {
         const userEmail = await User.findOne({email});
         if (userEmail) {
             return res.status(400).send('User already exists');
         }
         let user = new User(req.body);
+        const salt = await bcryptjs.genSalt(10);
+        user.password = await bcryptjs.hash(password, salt);
+
         await user.save();
         return res.send('User has been successfully created');
     } catch (error) {
