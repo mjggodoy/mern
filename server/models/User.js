c0e2cef7301453f13dab13376f50d23db62fca93
Maria <mjggodoy@gmail.com>
2020-10-16 20:31:46 +0200

    Setting the schema for users through mongoose.

---
+++ server/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const usersSchema = mongoose.Schema({
+    userName : {
+        type: String,
+        require: true,
+        trim: true
+    },
+    email : {
+        type: String,
+        require: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    insertDate: {
+        type : Data,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('User', usersSchema);
