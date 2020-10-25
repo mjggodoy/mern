abed6830551a1810d4e8202baf79a27295337247
Maria <mjggodoy@gmail.com>
2020-10-25 20:05:24 +0100

    Create new project from projectController.

--- server/controllers/projectsController.js
+++ server/controllers/projectsController.js
@@ -1,3 +1,12 @@
-exports.createProject = async (req, res) => {
+const Project = require('../models/Project');
 
+exports.createProject = async (req, res) => {
+    try {
+        const project = new Project(req.body);
+        await project.save();
+        return res.json(project);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('There was an error');
+    }
 }
