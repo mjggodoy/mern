ed81effe139e751cd91a03b8b0e49406b105dddf
Maria <mjggodoy@gmail.com>
2020-10-27 22:43:27 +0100

    Validate projects -> express-validator helps to check the project name. In case the project's name is empty, an error appears.

--- server/controllers/projectsController.js
+++ server/controllers/projectsController.js
@@ -1,6 +1,11 @@
 const Project = require('../models/Project');
+const {validationResult} = require('express-validator');
 
 exports.createProject = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
     try {
         const project = new Project(req.body);
         project.projectCreator = req.user.id;
--- server/routes/projects.js
+++ server/routes/projects.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 const projectController = require('./../controllers/projectsController');
 const authentication = require('../middleware/authentication');
+const {check} = require('express-validator');
 
 router.post('/',
     authentication,
+    [ 
+        check('projectName', 'The project name is mandatory').notEmpty()
+    ],
     projectController.createProject
 );
 module.exports = router;
