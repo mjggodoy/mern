1876c03da3938e6b8f134c598b1eaa60e5e49bd3
Maria <mjggodoy@gmail.com>
2020-11-03 20:04:31 +0100

    Create model schema with mongoose.

---
+++ server/models/Task.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const taskSchema = mongoose.Schema({
+    name : {
+        type: String,
+        required: true,
+        trim: true
+    },
+    status : {
+        type: String,
+        required: true,
+        trim: true,
+        default: 'In progress'
+    },
+    projectId : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'projects'
+    },
+    taskDate : {
+        type : Date,
+        default: Date.now()
+    }
+});
+module.exports = mongoose.model('tasks', taskSchema);
