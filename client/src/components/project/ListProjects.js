54cfa01a677457c84885ddeef3abd7b428366f0c
Maria <mjggodoy@gmail.com>
2021-01-31 23:30:34 +0100

    Include errors in list projects. When there is a problem such as -> token or something like that which is wrong, show an error from API before number of projects is zero. In case there is no error and number of projects is 0, return warning there is no projects.

--- client/src/components/project/ListProjects.js
+++ client/src/components/project/ListProjects.js
@@ -2,21 +2,28 @@ import React, {useContext, useEffect} from 'react';
 import Project from './Project';
 import ProjectContext from '../../context/projects/ProjectContext';
 import {CSSTransition, TransitionGroup} from 'react-transition-group';
+import AlertContext from './../../context/alerts/AlertContext';
 
 const ListProjects = () => {
     const projectsContext = useContext(ProjectContext);
-    const {projects, getProjects} = projectsContext;
+    const {alertAuth, projects, getProjects} = projectsContext;
+    const {alert, showAlert} = useContext(AlertContext);
 
     useEffect(() => {
+        if (alertAuth) {
+            showAlert(alertAuth.message);
+        }
         getProjects();
+        
     }, []);
 
-    if (projects.length === 0) {
+    if (projects.length === 0 && alertAuth.message == null) {
         return <h3 className="no-projects">There are no projects, start creating one project!</h3>;
     }
 
     return (
         <ul className="project-list">
+            {alert ? (<div className={`alert ${alert.category}`}>{alert.message}</div>) : null}
             <TransitionGroup>
                 {projects.map(project => (
                     <CSSTransition
