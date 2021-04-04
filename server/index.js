59b8378323432a0a3cbd58d06d70650ad562793b
Maria <mjggodoy@gmail.com>
2021-04-04 21:45:26 +0200

    Add port

--- server/index.js
+++ server/index.js
@@ -6,12 +6,12 @@ connectDB();
 app.use(cors());
 app.use(express.json({extended: true}));
 
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 app.use('/api/users', require('./routes/users'));
 app.use('/api/authUsers', require('./routes/authUser'));
 app.use('/api/projects', require('./routes/projects'));
 app.use('/api/tasks', require('./routes/task'));
 
-app.listen(PORT, () => {
-    console.log(`The app is running at this ${PORT}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`The app is running at this ${port}`);
 })
